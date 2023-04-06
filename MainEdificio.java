135c872922fb19ac359f9e0be37fa3701e56d8c2
JavierRamiro8 <jramiroc04@gmail.com>
2023-04-07 00:40:46 +0200

    Actualizado, Edificios

---
+++ MainEdificio.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+import programacion.negocios.Edificio.menu;
+public class MainEdificio{
+    public static void main(String[] args) {
+       System.out.println("Buenos dias, por favor selecciona una de las opciones del menú");
+        menu.ValidacionMenu();
+    }
+}
