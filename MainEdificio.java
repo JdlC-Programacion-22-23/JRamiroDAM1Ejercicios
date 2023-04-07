73048cc87ec4a79f2c5fd21fd058844d85cc356d
JavierRamiro8 <122380952+JavierRamiro8@users.noreply.github.com>
2023-04-07 11:46:33 +0200

    Delete MainEdificio.java No movido

--- MainEdificio.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-import programacion.negocios.Edificio.menu;
-public class MainEdificio{
-    public static void main(String[] args) {
-       System.out.println("Buenos dias, por favor selecciona una de las opciones del menú");
-        menu.ValidacionMenu();
-    }
-}
